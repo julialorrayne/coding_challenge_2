4e5e70eb1d4bf6e210c7a46d6f726804dff3f85a
Julia <lorrayne@usf.edu>
2025-01-26 15:44:20 -0500

    Task 1 - Store Inventory.

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+//Task 1: Working with Arrays
+let products = ['apple','banana','orange','strawberry','grapes'];
+products.push('mango'); //added mango to the end of the array
+
+products.pop();//removed the last element of the array(mango)
+
+console.log(products);
