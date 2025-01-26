b30c761fa82e58509da50ada9cf0d5162b8dff2d
Julia <lorrayne@usf.edu>
2025-01-26 16:06:35 -0500

    Task 2 - Student Scores.

--- cc_2.js
+++ cc_2.js
@@ -5,3 +5,11 @@ products.push('mango'); //added mango to the end of the array
 products.pop();//removed the last element of the array(mango)
 
 console.log(products);
+
+//Task 2: Accessing and Modifying Arrays
+let scores =[10,9.5,8,6,5];
+scores.splice(1,1,4); //replaced 9.5 with 4
+average = ((scores[0]+scores[1]+scores[2]+scores[3]+scores[4])/5); //calculated the average of scores
+
+console.log(scores);
+console.log(average);
